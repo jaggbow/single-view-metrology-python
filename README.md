5e4b10190c203b96204317ee81f35aae33ca929b
jaggbow <oussama.boussif@student.ecp.fr>
2021-02-19 19:01:05 +0100

    Fixed typo, removed folder and changed main

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Singlze View Metrology
+# Single View Metrology
 Python implementation of Single View Metrology with a flask web-app where people can import images and test the algorithm. This work was done in the context of a research project as part of the Visual Computing course at CentraleSupélec, 2020-2021.
 ![one piece original](assets/onepiece.jpg "one piece original")
 ![3d manga reconstruction](assets/3donepiece.PNG "3d manga reconstruction")
--- main.py
+++ main.py
@@ -15,33 +15,34 @@ import os
 #     'o': np.array([205, 280, 1])
 # }
 
-lines = {
-    "x": [[(1700, 3666), (3289, 2911)], [(1740, 3366), (3355, 2670)], [(259, 1921), (1553, 1784)]],
-    "y": [[(1700, 3666), (262, 2079)], [(1740, 3366), (259, 1921)], [(3355, 2670), (1553, 1784)]],
-    "z": [[(1700, 3666), (1740, 3366)], [(3289, 2911), (3355, 2670)], [(262, 2079), (259, 1921)]]
-}
-refpoints = {
-    'x': np.array([3289, 2911, 1]).reshape(-1, 1),
-    'y': np.array([262, 2079, 1]).reshape(-1, 1),
-    'z': np.array([1740, 3366, 1]).reshape(-1, 1),
-    'o': np.array([1700, 3666, 1])
-}
-img = cv2.imread('img/onepiece.jpg')
-singlevm = SingleViewM(img, endpoints=lines, refpoints=refpoints)
+if __name__ == 'main':
+    lines = {
+        "x": [[(1700, 3666), (3289, 2911)], [(1740, 3366), (3355, 2670)], [(259, 1921), (1553, 1784)]],
+        "y": [[(1700, 3666), (262, 2079)], [(1740, 3366), (259, 1921)], [(3355, 2670), (1553, 1784)]],
+        "z": [[(1700, 3666), (1740, 3366)], [(3289, 2911), (3355, 2670)], [(262, 2079), (259, 1921)]]
+    }
+    refpoints = {
+        'x': np.array([3289, 2911, 1]).reshape(-1, 1),
+        'y': np.array([262, 2079, 1]).reshape(-1, 1),
+        'z': np.array([1740, 3366, 1]).reshape(-1, 1),
+        'o': np.array([1700, 3666, 1])
+    }
+    img = cv2.imread('img/onepiece.jpg')
+    singlevm = SingleViewM(img, endpoints=lines, refpoints=refpoints)
 
-proj_matrix = singlevm.findProjectionMatrix()
-H_xy = proj_matrix[:, [0, 1, 3]].copy()
-H_yz = -proj_matrix[:, [1, 2, 3]].copy()
-H_xz = -proj_matrix[:, [0, 2, 3]].copy()
+    proj_matrix = singlevm.findProjectionMatrix()
+    H_xy = proj_matrix[:, [0, 1, 3]].copy()
+    H_yz = -proj_matrix[:, [1, 2, 3]].copy()
+    H_xz = -proj_matrix[:, [0, 2, 3]].copy()
 
-if not os.path.isdir('textures'):
-    os.mkdir('textures')
+    if not os.path.isdir('textures'):
+        os.mkdir('textures')
 
-frame_xy = cv2.warpPerspective(img, H_xy, img.shape[:2], flags=cv2.WARP_INVERSE_MAP)
-cv2.imwrite('textures/xy.jpg', frame_xy)
+    frame_xy = cv2.warpPerspective(img, H_xy, img.shape[:2], flags=cv2.WARP_INVERSE_MAP)
+    cv2.imwrite('textures/xy.jpg', frame_xy)
 
-frame_xz = cv2.warpPerspective(img, H_xz, img.shape[:2], flags=cv2.WARP_INVERSE_MAP)
-cv2.imwrite('textures/xz.jpg', frame_xz)
+    frame_xz = cv2.warpPerspective(img, H_xz, img.shape[:2], flags=cv2.WARP_INVERSE_MAP)
+    cv2.imwrite('textures/xz.jpg', frame_xz)
 
-frame_yz = cv2.warpPerspective(img, H_yz, img.shape[:2], flags=cv2.WARP_INVERSE_MAP)
-cv2.imwrite('textures/yz.jpg', frame_yz)
+    frame_yz = cv2.warpPerspective(img, H_yz, img.shape[:2], flags=cv2.WARP_INVERSE_MAP)
+    cv2.imwrite('textures/yz.jpg', frame_yz)
